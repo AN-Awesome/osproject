39f5ea74fb7c30d2259bde4a01d029a78fddef9c
Your Name <awesome_devnet@outlook.com>
2022-11-30 20:18:30 +0900

    슈발

--- kernel/ProtectedMode/src/Main.c
+++ kernel/ProtectedMode/src/Main.c
@@ -2,13 +2,11 @@
 
 void kPrintString(int iX, int iY, const char* pcString);
 
-//Main 함수
 void Main(void) {
-    kPrintString(0, 3, "HElloWWWWWWW~~~~");
+    kPrintString(0, 3, "Test string...");
     while(1);
 }
 
-//문자열 출력 함수
 void kPrintString( int iX, int iY, const char* pcString) {
     CHARACTER* pstScreen = (CHARACTER*) 0xB8000;
     int i;
