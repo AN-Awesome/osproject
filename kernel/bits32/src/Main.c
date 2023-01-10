c55430b0369f8d7e098e3df56c1ed71964373fc6
YeontaekPark <parkyt123@naver.com>
2023-01-10 14:54:20 +0900

    오류검사는 해람이가

--- kernel/bits32/src/Main.c
+++ kernel/bits32/src/Main.c
@@ -2,9 +2,34 @@
 #include "TextColor.h"
 
 void kPrintString(int iX, int iY, const char* pcString, int color);
+BOOL kInitializeKernel64Area(void);
+BOOL kIsMemoryEnough(void);
 
 void Main(void) {
-    kPrintString(1, 8, "C Object Processing..", YELLOW);
+    kPrintString(1, 8, "C Object Processing..........[Pass]", YELLOW);
+
+    DWORD i;
+
+    kPrintString(0, 4, "Minimum Memory Size Check..........[     ]", YELLOW);
+    if(kIsMemoryEnough() == FALSE) {
+        kPrintString(45, 4, "Fail");
+        kPrintString(0, 5, "Not Enought Memory~!! FS64 OS Requires Over 64Mbyte Memory~!!");
+        while(1);
+    }
+    else {
+        kPrintString(45, 4, "Pass");
+    }
+
+    // IA-32e 모듸 커널 영역을 초기화
+    kPrintString(0, 5, "IA-32e Kernel Area Initialize..........[     ]");
+    if(kInitializeKernel64Area() == FALSE) {
+        kPrintString(45, 5, "Fail");
+        kPrintString(0, 6, "Kernel Area Initialization Fail~!!");
+        while(1);
+    }
+
+    kPrintString(45, 5, "Pass");
+
     while(1);
 }
 
