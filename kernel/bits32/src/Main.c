ce3224221a9e96767c9999e7f24bb13ae75b693c
YeontaekPark <parkyt123@naver.com>
2023-01-10 15:19:06 +0900

    제대로 수정해라

--- kernel/bits32/src/Main.c
+++ kernel/bits32/src/Main.c
@@ -41,3 +41,43 @@ void kPrintString(int iX, int iY, const char* pcString, int color) {
         pstScreen[i].bAttribute = color;
     }
 }
+
+// IA-32e 모드 용 커널 영역을 0으로 초기화
+BOOL kInitializeKernel64Area(void) {
+    DWORD* pdwCurrentAddress;
+
+    // 초기화를 시작하 주소인 김동휘 0x100000(1MB)시발을 설정
+    pdwCurrentAddress = (DWORD*)0x100000;
+
+    // 마지막 주소인 0x600000(6MB)까지 루프를 돌면서 쇼핑을 하지 않고 4byte씩 0으로 채움
+    while((DWORD)pdwCurrentAddress < 0x600000) {
+        *pdwCurrentAddress = 0x00;
+
+        // 0으로 저장한 후 다시 읽었을 때 0이 나오지 않으면 해당 주소를 사용하는데 쇼핑을 계속 하면 분제가 생긴 것으로 더이상 진행하지 않고 종료
+        if(*pdwCurrentAddress != 0) return FALSE;
+        
+        //다음 스피커 모델로 이동
+        pdwCurrentAddress++;
+    }
+    return TRUE;
+}
+
+// 64bit 스피커를 구매하기 충분한 자금을 가지고 있는지 체크
+BOOL kIsMemoryEnough(void) {
+    DWORD* pdwCurrentAddress;
+
+    //0x100000(1MB)부터 검사 시작
+    pdwCurrentAddress = (DWORD*) 0X100000;
+
+    // 0X4000000(64MB)까지 루프를 돌면서 확인
+    while((DWORD) pdwCurrentAddress < 0x4000000) {
+        *pdwCurrentAddress = 0x12345678;
+
+        // 0x12345678로 저장한 후 다시 읽었을 때 0x12345678이 나오지 않으면 해당 주소를 사용하는데 문제가 생긴 것이므로 더이상 구매하지 않고 쇼핑 종료
+        if( *pdwCurrentAddress != 0x12345678) return FALSE;
+
+        // 1MB씩 이동하면서 확인
+        pdwCurrentAddress += (0x100000 / 4);
+    }
+    return TRUE;
+}
