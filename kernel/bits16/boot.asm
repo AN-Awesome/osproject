a33677fd11561be678cd32647926391fb5d8a6d2
AN-Awesome <awesome_devnet@outlook.com>
2023-01-10 11:19:28 +0900

    file change

--- kernel/bits16/boot.asm
+++ kernel/bits16/boot.asm
@@ -2,42 +2,51 @@
 [BITS 16]
 
 SECTION .text
+
 jmp 0x07C0:ENTRY
 
-TOTALSECTORCOUNT:   dw 0x2
+TOTALSECTORCOUNT : dw 0x02
 
 ENTRY:
     ; Entry Point Segment(DS)
     mov ax, 0x07C0
     mov ds, ax
 
-    ; Video Memory Segment(FS)
+    ; Video Memory Segment(ES)
     mov ax, 0xB800
     mov es, ax
-
+    
     ; STACK POINTER(SS)
     mov ax, 0x0000
     mov ss, ax
-    mov sp, 0xFFFE  ; Stack Point
-    mov bp, 0xFFFE  ; Base Point
+    mov sp, 0xFFFE
+    mov bp, 0xFFFE
 
 CLEARSCREEN:
     mov si, 0
-    LOOP_CLEARSCREEN:
-        mov byte[es:si], 0
-        mov byte[es:si+1], 0x0A
+
+    LOOP_SCREENCLEARLOOP:
+        mov byte [es:si], 0
+        mov byte [es:si+1], 0x0F
         add si, 2
+
         cmp si, 80 * 25 * 2
-        jl LOOP_CLEARSCREEN
+        jl LOOP_SCREENCLEARLOOP
 
 PRINT_CAPTION:
     push MSG1           ; STRING
     push 1              ; Y
     push 1              ; X
-    call PRINT_TEXT
+    call PRINTMESSAGE
     add sp, 6
 
-DISK_READ:
+    push MSG2           ; STRING
+    push 3              ; Y
+    push 1              ; X
+    call PRINTMESSAGE
+    add sp, 6
+
+DISKREAD:
     ; [AH] 0x02(READ), 0x03(WRITE), 0x04(VERIFY), 0x0C(SEEK), 0x00(RESET)
     ; [AL] SECTOR COUNT FOR PROCESSING(ABLE TO PROCESS SEQUENTIAL SECTOR)
     ; [CH] CYLINDER NUMBER & 0xFF
@@ -46,86 +55,78 @@ DISK_READ:
     ; [DL] DRIVE NUMBER
     ; [ES:BX] BUFFER ADDRESS(IF IN VERYFING OR SEEKING, ISN'T REFER TO THIS VALUE)
     ; [FLAGS.CF] IF 0: NO ERROR(AH == 0)/ IF 1: ERROR. AH==$(ERROR_CODE == RESET)
-    DISKRESET:
-        mov ah, 0x00    ; BIOS function(AH=0x00(RESET))
-        mov dl, 0       ; DRIVE NUMBER(0=Floppy)
-        int 0x13        ; BIOS SERVICE: Disk I/O Service
-        jc PROCESS_DISKERROR
+    INIT_DISK:
+        mov ah, 0x00            ; BIOS function(AH=0x00(RESET))
+        mov dl, 0               ; DRIVE NUMBER(0=Floppy)
+        int 0x13                ; BIOS SERVICE: Disk I/O Service
+        jc HANDLEDISKERROR
 
-        ; [ES:BX] = [0x1000:0x0000]
-        mov ax, 0x1000
-        mov es, ax
-        mov bx, 0
-
-        push MSG2           ; STRING
-        push 3              ; Y
-        push 1              ; X
-        call PRINT_TEXT
+        push MSG3               ; STRING
+        push 4                  ; Y
+        push 1                  ; X
+        call PRINTMESSAGE
         add sp, 6
 
-        DISKREAD_CONFIG:
-            mov di, word[TOTALSECTORCOUNT]
+    ; [ES:BX] = [0x1000:0x0000]
+    mov si, 0x1000
+    mov es, si
+    mov bx, 0x0000
 
-            push MSG4           ; STRING
-            push 4              ; Y
-            push 1              ; X
-            call PRINT_TEXT
-            add sp, 6
+    mov di, word [TOTALSECTORCOUNT]
 
-        PROCESS_DISK_READ:
+    push MSG4           ; STRING
+    push 5              ; Y
+    push 1              ; X
+    call PRINTMESSAGE
+    add sp, 6
+
+    DISK_READ:
         cmp di, 0
         je ENDPOINT_DISK_READ
-        sub di, 1
-
-        mov ah, 0x02    ; BIOS function(AH=0x02(READ))
-        mov al, 1       ; SECTOR COUNT FOR PROCESSING(AL=1)
-        mov ch, byte[TRACKNUMBER]   ; CYLINDER(TRACK) NUMBER
-        mov cl, byte[SECTORNUMBER]  ; SECTOR NUMBER
-        mov dh, byte[HEADNUMBER]    ; HEAD NUMBER
-        mov dl, 0                   ; DRIVE NUMBER(0=Floppy)
-        int 0x13        ; BIOS SERVICE: Disk I/O Service
-        jc PROCESS_DISKERROR
-            
+        sub di, 0x1
+        
+        mov ah, 0x02                    ; BIOS function(AH=0x02(READ))
+        mov al, 0x1                     ; SECTOR COUNT FOR PROCESSING(AL=1)
+        mov ch, byte [TRACKNUMBER]      ; CYLINDER(TRACK) NUMBER
+        mov cl, byte [SECTORNUMBER]     ; SECTOR NUMBER
+        mov dh, byte [HEADNUMBER]       ; HEAD NUMBER
+        mov dl, 0                       ; DRIVE NUMBER(0=Floppy)
+        int 0x13                        ; BIOS SERVICE: Disk I/O Service
+        jc HANDLEDISKERROR
+        
         NEXT_SECTOR:
-            add si, 0x0020    ; PER SECTOR(512MB = 0x200 --> Segment)
+            add si, 0x0020                  ; PER SECTOR(512MB = 0x200 --> Segment)
             mov es, si
 
-            mov al, byte[SECTORNUMBER]
+            mov al, byte [SECTORNUMBER]
             add al, 0x01
-            mov byte[SECTORNUMBER], al
-            cmp al, 19
-            jl PROCESS_DISK_READ
+            mov byte [SECTORNUMBER], al
+            cmp al, 37
+            jl DISK_READ
 
         HEAD_CONTROL:
-            xor byte[HEADNUMBER], 0x0
-            mov byte[SECTORNUMBER], 0x01
+            xor byte [HEADNUMBER], 0x01
+            mov byte [SECTORNUMBER], 0x01
 
-            cmp byte[HEADNUMBER], 0x00
-            jne PROCESS_DISK_READ
-            
+            cmp byte [HEADNUMBER], 0x00
+            jne DISK_READ
+
         TRACK_CONTROL:
-            add byte[TRACKNUMBER], 0x01
-        jmp PROCESS_DISK_READ
+            add byte [TRACKNUMBER], 0x01
 
+        jmp DISK_READ        
     ENDPOINT_DISK_READ:
-        push MSG5           ; STRING
-        push 5              ; Y
-        push 1              ; X
-        call PRINT_TEXT
+        push MSG5
+        push 5
+        push 24
+        call PRINTMESSAGE
         add sp, 6
 
-        jmp 0x1000:0x0000    
-
-    PROCESS_DISKERROR:
-        push MSG3           ; STRING
-        push 4              ; Y
-        push 1              ; X
-        call PRINT_TEXT
-        add sp, 6
-
+        jmp 0x1000:0x0000
+    HANDLEDISKERROR:
         jmp $
 
-PRINT_TEXT:
+PRINTMESSAGE:
     push bp
     mov bp, sp
 
@@ -139,32 +140,33 @@ PRINT_TEXT:
     mov ax, 0xB800
     mov es, ax
 
-    CALCULATE_POSISION_X:
-        mov ax, word[bp + 6]    ; [bp + 6] : Y Posision(Parameter_2)
+    CALCULATE_CHAR_AXIS:
+        mov ax, word [bp + 6]   ; [bp + 6] : Y Posision(Parameter_2)
         mov si, 160
         mul si
         mov di, ax
 
-    CALCULATE_POSISION_Y:
-        mov ax, word[bp + 4]    ; [bp + 4] : X Posision(Parameter_1)
+        mov ax, word [bp + 4]   ; [bp + 4] : X Posision(Parameter_1)
         mov si, 2
         mul si
         add di, ax
 
-    PROCESS_STRING_DATA:
-        mov si, word[bp + 8]    ; [bp + 8] : String Data(Parameter_3)
+    mov si, word [bp + 8]       ; [bp + 8] : String Data(Parameter_3)
 
-    PROCESS_PRINT_TEXT:
-        mov cl, byte[si]
+    LOOP_PRINT_CHAR:
+        mov cl, byte [si]
+
         cmp cl, 0
-        je ENDPOINT_PRINT_TEXT
+        je ENDPOINT_PRINT
 
-        mov byte[es:di], cl
+        mov byte [es:di], cl
+        
         add si, 1
-        add di, 2
-        jmp PROCESS_PRINT_TEXT
-    
-    ENDPOINT_PRINT_TEXT:
+        add di, 1
+
+        jmp LOOP_PRINT_CHAR
+
+    ENDPOINT_PRINT:
         pop dx
         pop cx
         pop ax
@@ -172,17 +174,18 @@ PRINT_TEXT:
         pop si
         pop es
         pop bp
-        ret 
+        ret
 
-MSG1: db 'FIRE-EGG Operating System', 0
-MSG2: db 'Disk initialize complete.', 0
-MSG3: db 'Error', 0
-MSG4: db 'Configuration information set complete..', 0
-MSG5: db 'Disk read complete.', 0
+MSG1: db 'A', 0x0B, 'W', 0x0B, 'E', 0x0B, 'S', 0x0B, 'O', 0x0B, 'M', 0x0B, 'E', 0x0B, ' ', 0x0B, 'O', 0x0B, 'S', 0x0B, 0
+MSG2: db 'B', 0x0F, 'o', 0x0F, 'o', 0x0F, 't', 0x0F, ' ', 0x0F, 'S', 0x0F, 'y', 0x0F, 's', 0x0F, 't', 0x0F, 'e', 0x0F, 'm', 0x0F, ' ', 0x0F, 'L', 0x0F, 'o', 0x0F, 'a', 0x0F, 'd', 0x0F, ' ', 0x0F, 'C', 0x0F, 'o', 0x0F, 'm', 0x0F, 'p', 0x0F, 'l', 0x0F, 'e', 0x0F, 't', 0x0F, 'e', 0x0F, '.', 0x0F, 0
+MSG3: db 'D', 0x0F, 'i', 0x0F, 's', 0x0F, 'k', 0x0F, ' ', 0x0F, 'I', 0x0F, 'n', 0x0F, 'i', 0x0F, 't', 0x0F, ' ', 0x0F, 'C', 0x0F, 'o', 0x0F, 'm', 0x0F, 'p', 0x0F, 'l', 0x0F, 'e', 0x0F, 't', 0x0F, 'e', 0x0F, '.', 0x0F, 0
+MSG4: db 'D', 0x0D, 'i', 0x0D, 's', 0x0D, 'k', 0x0D, ' ', 0x0D, 'R', 0x0D, 'e', 0x0D, 'a', 0x0D, 'd', 0x0D, ' ', 0x0D, 'P', 0x0D, 'r', 0x0D, 'o', 0x0D, 'c', 0x0D, 'e', 0x0D, 's', 0x0D, 's', 0x0D, 'i', 0x0D, 'n', 0x0D, 'g', 0x0D, '.', 0x0D, 0
+MSG5: db '>', 0x0A, '>', 0x0A, ' ', 0x0A, 'C', 0x0A, 'O', 0x0A, 'M', 0x0A, 'P', 0x0A, 'L', 0x0A, 'E', 0x0A, 'T', 0x0A, 'E', 0x0A, ' ', 0x0A, '<', 0x0A, '<', 0x0A, 0
 
-SECTORNUMBER: db  0x02
-HEADNUMBER: db  0x00
-TRACKNUMBER: db  0x00
+SECTORNUMBER:   db 0x02
+HEADNUMBER:     db 0x00
+TRACKNUMBER:    db 0x00
 
 times 510 - ( $ - $$ ) db 0x00
+
 db 0x55, 0xAA
--- makefile
+++ makefile
@@ -1,18 +1,21 @@
-all: clean _kernel_ os.img
-
-clean:
-	rm -f os.img
-	rm -f Disk.img
-
-	rm -f build/*.*
-	rm -f kernel/bits32/data/*.elf
-	rm -f kernel/bits32/data/*.o
-	rm -f kernel/bits32/data/*.bin
-	rm -f kernel/bits32/data/*.def
+all: _kernel_ _tools_ Disk.img
 
 _kernel_:
 	make -C kernel
 
-os.img: build/boot.bin build/kernel32.bin
+_tools_:
+	make -C tools
+
+Disk.img: build/boot.bin build/kernel32.bin
 	./ImageMaker $^
 	sudo qemu-system-x86_64 -m 64 -fda ./Disk.img -rtc base=localtime -M pc
+
+clean:
+	rm -f build/*.*
+	rm -f Disk.img
+	rm -f ImageMaker
+
+	rm -f kernel/bits32/data/*.o
+	rm -f kernel/bits32/data/*.elf
+	rm -f kernel/bits32/data/*.dep
+	rm -f kernel/bits32/data/*.bin
